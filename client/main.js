346ce5608ac51e83adbb1751d0bc0f2169bec05d
cloud1992 <arturocollado@ratherlabs.com>
2023-11-04 12:26:26 -0300

    :sparkles: added socket response

--- client/main.js
+++ client/main.js
@@ -1,2 +1,19 @@
-var io = require('socket.io-client');
 var socket = io.connect('http://192.168.0.59:6677', {'forceNew':true});
+
+socket.on('messages', function(data){
+    console.log(data);
+    render(data);
+});
+
+function render(data){
+    var html = data.map(function(message, index){
+        return (`
+            <div class="message">
+                <strong>${message.nickname}</strong>
+                <p>${message.text}</p>
+            </div>
+        `);
+    });
+
+    document.getElementById('messages').innerHTML = html;
+}
--- server/index.js
+++ server/index.js
@@ -9,8 +9,16 @@ app.get('/hola', function(req, res){
     res.status(200).send('Hola');
 });
 
+var messages = [{
+    id:1,
+    text: "Welcome to private chat",
+    nickname: 'Bot - cloud1992'
+}];
+
 io.on('connection', function(socket){
-    console.log('The device IP is: ' + socket.handshake.address + 'has been connected');
+    console.log('The device IP is: ' + socket.handshake.address + ' has been connected');
+
+    socket.emit('messages', messages);
 });
 
 server.listen(6677, function(){
